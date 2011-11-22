0b9484f3e732af1c88784b882ab93cb63197c4bb
Panu P <panu@taguchimail.com>
2011-11-23 10:42:54 +1100

    Update release version to 1.0.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="crone",
-    version="1.0",
+    version="1.0.1",
     description="A little extension to cron",
     author="Panu P",
     author_email="panuph@gmail.com",
