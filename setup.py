ba41067019488bc33b9ce4ad26a50a6c5ccbb07e
Panu P <panu@taguchimail.com>
2011-11-23 11:06:21 +1100

    Finally found a way to deal with meta data.

--- setup.py
+++ setup.py
@@ -3,9 +3,18 @@ from distutils.core import setup
 setup(
     name="crone",
     version="1.0.1",
-    description="A little extension to cron",
     author="Panu P",
     author_email="panuph@gmail.com",
+    maintainer="Panu P",
+    maintainer_email="panuph@gmail.com",
     url="https://github.com/panuph/crone",
+    description="A little extension to cron",
+    long_description="Like cron but you can further indicate the timezone, " + \
+        "the period and interval to run a job (tested on python 2.6 but " + \
+        "should be fine to run with older python releases).",
+    download_url="https://github.com/panuph/crone/downloads",
+    classifiers=["Programming Language :: Python", "Operating System :: POSIX"],
+    platforms=["python"],
+    license="Freeware",
     py_modules=["crone"]
 )
